acc5bb35962c6c9fe57a7f438178223b001a48b4
Dclasyc <Akarigbooladipo@gmail.com>
2024-06-08 01:24:24 -0600

    Rendering questions
    Rendering  quiz questions

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import Answer from "./Answer";
+import { QuizContext } from "../contexts/quiz";
 
-const Question = () =>{
+const Question = ({ questions }) => {
+    const [quizState] = useContext(QuizContext);
+    const currentQuestion = quizState.questions[quizState.currentQuestionIndex];
     return (
     <div>
-        <div className="question">Questions</div>
+        <div className="question">{currentQuestion.question}</div>
         <div className="answers">
             <Answer />
             <Answer />
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,27 +1,18 @@
-import { useReducer } from "react";
+import { useContext } from "react";
 import Question from "./Question";
-import { type } from "@testing-library/user-event/dist/type";
+import { QuizContext } from "../contexts/quiz";
 
-const initialState ={
-    currentQuestionIndex: 0,
-    questions: [],
-};
 
-const reducer = (state, action) => {
-    if(action.type === "NEXT_QUESTION"){
-        return{state, currentQuestionIndex: state.currentQuestionIndex + 1}
-    }
-    return state;
-}
 
 const Quiz = () =>{
-    const[state, dispatch] = useReducer(reducer, initialState);
-    console.log("render", state);
-
+    const [quizState, dispatch] = useContext(QuizContext);
+    console.log("quizState", quizState)
     return (
         <div className="quiz">
             <div>
-                <div className="score">Question 1/8</div>
+                <div className="score">
+                    Question {quizState.currentQuestionIndex + 1}/{quizState.questions.length}
+                </div>
                 <Question />
                 <div className="next-button" onClick={() => dispatch({type: 'NEXT_QUESTION'})}>
                     Next Question
